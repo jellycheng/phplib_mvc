d4573f709227ddfe4a439b37461af72abe183b8c
jellycheng <42282367@qq.com>
2013-10-22 17:45:51 +0800

    minsys

---
+++ minsys/config/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+			'encoding'=>'utf-8',
+			'var_mod'=>'mod',
+			'var_act'=>'act',
+			'default_timezone'=>'Asia/Shanghai',
+			);
+	
--- minsys/public/index.php
+++ minsys/public/index.php
@@ -2,8 +2,7 @@
 $_env_name = isset($_SERVER['ENV_NAME']) ? $_SERVER['ENV_NAME'] : get_cfg_var('env_name');
 define('ENVIRONMENT', $_env_name ? $_env_name : 'dev');
 
-switch (ENVIRONMENT)
-{
+switch (ENVIRONMENT) {
 	case 'dev':
 	default:	
 			error_reporting(E_ALL | E_STRICT);
@@ -11,5 +10,6 @@ switch (ENVIRONMENT)
 		break;
 }
 
+define('APP_ROOT', dirname(__DIR__) . '/');
 require dirname(dirname(__DIR__)) . '/phplib/_init.php';
-\phplib\core\app::init(array('encoding'=>'utf-8'))->run();
+\phplib\core\webApp::init(require APP_ROOT . 'config/config.php')->run();
