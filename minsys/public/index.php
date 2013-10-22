5bf3b9701fe70ac7440dca55b11630e7eaed413e
jellycheng <42282367@qq.com>
2013-10-22 09:55:24 +0800

    minsys

---
+++ minsys/public/index.php
@@ -0,0 +1,15 @@
+<?php
+$_env_name = isset($_SERVER['ENV_NAME']) ? $_SERVER['ENV_NAME'] : get_cfg_var('env_name');
+define('ENVIRONMENT', $_env_name ? $_env_name : 'dev');
+
+switch (ENVIRONMENT)
+{
+	case 'dev':
+	default:	
+			error_reporting(E_ALL | E_STRICT);
+			ini_set('display_errors', 1);
+		break;
+}
+
+require dirname(dirname(__DIR__)) . '/phplib/_init.php';
+\phplib\core\app::init(array('encoding'=>'utf-8'))->run();
