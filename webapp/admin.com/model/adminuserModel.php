26604a8e1a868ab4c25061dac2169992cee2cf44
jellycheng <42282367@qq.com>
2013-10-23 17:35:44 +0800

    webapp

---
+++ webapp/admin.com/model/adminuserModel.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 
+ * 子项目业务模型,数据模型
+ * 格式:namespace app\model\子项目目录去掉点; 如namespace app\model\admincom;
+ * @copyright	(c) 2013-2099 All Rights Reserved
+ * @author		jellycheng <42282367@qq.com>
+ * @created		2013-10-23
+ * @version		$Id$
+ */
+namespace app\model\admincom;
+
+use phplib\core\model;
+
+class adminuserModel extends model {
+
+	
+	public function index() {
+		echo __FILE__;
+	
+	}
+}
