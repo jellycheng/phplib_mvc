137b0949e7f36e5ea5246d7b1b6f0e2973bfb6bb
jellycheng <42282367@qq.com>
2013-10-23 15:02:09 +0800

    core

--- phplib/core/webApp.php
+++ phplib/core/webApp.php
@@ -14,13 +14,40 @@ class webApp extends app {
 
 	static protected function _init() {
 		dispatcher::start();
+		
 	}
 
 	static protected function _run() {
-		
+		$className = $partFileName = '';
+		$_config = \phplib\phplib::getEnv('config');
 		$mod = \phplib\phplib::getEnv('mod');
 		$act = \phplib\phplib::getEnv('act');
-		echo "mod={$mod} act={$act}   todo...";
+		
+		
+		$_controller_namespace = '\\';
+		if(isset($_config['controller_namespace']) && $_config['controller_namespace']) {
+			$_controller_namespace = $_config['controller_namespace'];
+		}
+		if(preg_match('/_/', $mod)) {
+			$partFileName = str_replace('_', '/', $mod);
+			$className = substr($mod, strrpos($mod, '_') + 1) . 'Control';
+		} else {
+			$partFileName = $mod;
+			$className = $mod . 'Control';
+		}
+		
+		$_file = APP_ROOT . "control/{$partFileName}Control.php";
+		if(file_exists($_file)) {
+			include $_file;
+			$className = rtrim($_controller_namespace, '\\') . '\\' . $className;
+			$_obj = new $className();
+			//$_obj->$act();
+			call_user_func(array(&$_obj,$act));
+		} else {
+			#404 todo
+			
+		}
+
 	}
 
 }
