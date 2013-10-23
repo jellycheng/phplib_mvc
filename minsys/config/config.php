092a5ceb5dd6b376b8e2be03d64ac6769fe10dd9
jellycheng <42282367@qq.com>
2013-10-23 15:01:33 +0800

    minsys

--- minsys/config/config.php
+++ minsys/config/config.php
@@ -1,8 +1,14 @@
 <?php
 return array(
 			'encoding'=>'utf-8',
+			'default_timezone'=>'Asia/Shanghai',
+			'controller_namespace'=>'\\app\\control',
+			/* 系统变量名称设置 */
 			'var_mod'=>'mod',
 			'var_act'=>'act',
-			'default_timezone'=>'Asia/Shanghai',
+			'VAR_PATHINFO'=> 's',
+			
+			/* 数据库设置 */
+			
 			);
 	
---
+++ minsys/control/indexControl.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\control;
+
+use phplib\core\control;
+
+class indexControl extends control {
+
+	
+	public function index() {
+		echo __FILE__;
+	
+	}
+}
