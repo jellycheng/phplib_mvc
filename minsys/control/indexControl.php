b30b625e0f5f6ab41191ad44d1d038015a0ea6d3
jellycheng <42282367@qq.com>
2013-11-05 13:30:59 +0800

    control

--- minsys/control/indexControl.php
+++ minsys/control/indexControl.php
@@ -13,5 +13,11 @@ class indexControl extends control {
         print_r(get_included_files());
 
         print_r(\phplib\phplib::getEnv());
+		echo <<<EOT
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fb5b289b3bab0100d7eb6f6f3f6cb213a' type='text/javascript'%3E%3C/script%3E"));
+</script>
+EOT;
 	}
 }
